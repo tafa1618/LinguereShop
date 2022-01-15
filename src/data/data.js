596be3ba8eec3121cdda53392d6cc40fcfc4a82b
tafa1618 <64489795+tafa1618@users.noreply.github.com>
2022-01-15 14:30:43 +0000

    set and up the project

---
+++ src/data/data.js
@@ -0,0 +1,113 @@
+const Products = [
+  {
+    id: '1',
+    name: 'Robe noire ',
+    image:
+      'https://i.ibb.co/DwbpbrC/Whats-App-Image-2021-11-24-at-22-15-43.jpg',
+    description: 'Robe Noire',
+    category: 'Robe',
+    price: 5000,
+  },
+  {
+    id: '2',
+    name: 'Robe Longue',
+    image:
+      'https://i.ibb.co/8gvJdH0/Whats-App-Image-2021-11-24-at-22-15-41-1.jpg',
+    description:
+      'Robe Longue Taille standard couleur Rouge Noire Blanc Bleu rose.',
+    category: 'Robe',
+    price: 6000,
+  },
+  {
+    id: '3',
+    name: 'Nuisette 3 pièces ',
+    image: 'https://i.ibb.co/D19Q7yf/1.jpg',
+    description:
+      'Nuisette taille standard couleur Bleu Bic  -Bleu-turkoise -rouge -noir -rose -blanc ',
+    category: 'Nuisette',
+    price: 5000,
+  },
+  {
+    id: '4',
+    name: 'Nuisette  ',
+    image: 'https://i.ibb.co/fpCwXYQ/2.jpg',
+    description: 'Couleur Verte noir blanc rose rouge rouge blanc',
+    category: 'Nuisette',
+    price: 5000,
+  },
+  {
+    id: '5',
+    name: 'Nuisette',
+    image: 'https://i.ibb.co/b1F8FWk/3.jpg',
+    description: 'Couleur Rouge noir blanc rose bleu-turkoise',
+    category: 'Nuisette',
+    price: 5000,
+  },
+  {
+    id: '6',
+    name: 'Nuisette 3 pièces  ',
+    image: 'https://i.ibb.co/3vd129P/4.jpg',
+    description: 'Taille standard Couleur Rouge saumo noire bleu-bic beige',
+    category: 'Nuisette',
+    price: 6000,
+  },
+  {
+    id: '7',
+    name: 'Nuisette',
+    image: 'https://i.ibb.co/rK9xWJZ/5.jpg',
+    description:
+      'Taille standard Couleur Saumo rouge noire blanc bleu-turkoise',
+    category: 'Nuisette',
+    price: 5000,
+  },
+  {
+    id: '8',
+    name: 'Nuisette',
+    image: 'https://i.ibb.co/LDFf5dX/6.jpg',
+    description:
+      'Taille standard   Couleur Rose-blanc rouge-blanc blanc- saumo- bleu- turkoise noire',
+    category: 'Nuisette',
+    price: 5000,
+  },
+  {
+    id: '9',
+    name: 'Nuisette 3 pièces',
+    image:
+      'https://i.ibb.co/56XwPK2/Whats-App-Image-2021-11-24-at-22-15-36-1.jpg',
+    description:
+      'Taille standard   Couleur  Rouge-noire-blanc-rose-bleu-bic-bleu-turkoise',
+    category: 'Nuisette',
+    price: 7000,
+  },
+  {
+    id: '10',
+    name: 'Short',
+    image:
+      'https://i.ibb.co/dpRfQp9/Whats-App-Image-2021-11-24-at-22-03-19-1.jpg',
+    description:
+      'Taille standard  Couleur  Saumon-Noire   Bleu turkoise- noir  Rouge -noire   Noire-blanc Bleu-bic-noire Beige-noir',
+    category: 'Short',
+    price: 4000,
+  },
+  {
+    id: '11',
+    name: 'Short',
+    image:
+      'https://i.ibb.co/1bRrhLM/Whats-App-Image-2021-11-24-at-22-03-19.jpg',
+    description:
+      'Taille standard  Couleur  Saumon-Noire   Bleu turkoise- noir  Rouge -noire   Noire-blanc Bleu-bic-noire Beige-noir',
+    category: 'Short',
+    price: 4000,
+  },
+  {
+    id: '12',
+    name: 'Ensemble Short',
+    image:
+      'https://i.ibb.co/6bfRP7q/Whats-App-Image-2021-11-24-at-22-15-38-1.jpg',
+    description: 'Taille standard Couleur  Saumo-noire - blanc-rouge-bleu-',
+    category: 'Short',
+    price: 4000,
+  },
+];
+
+export default Products;
